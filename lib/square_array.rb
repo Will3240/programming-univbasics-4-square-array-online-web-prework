bd44c7203b6f1e2b634caeb471be67250bca5fc5
William <teststuffreally@gmail.com>
2019-09-29 14:34:56 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
-end
+  counter = 0
+  squares=[]
+  while array[counter] do
+  squares.push(array[counter]*array[counter])  
+  counter +=1
+  end
+  return squares
+  
+end  
+#   counter = 0
+# pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
+ 
+# while pets[counter] do
+#   puts pets[counter]
+#   counter += 1
+# end
